68408b89af4cc37f9b369180107bea56126105ff
CuartoHokage <jaime199505@hotmail.com>
2018-06-19 22:09:23 -0500

    f

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports={
 	port: process.env.PORT || 3000,
-	db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+	//db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+	db: process.env.MONGODB || 'mongodb://12qwaszx:12qwaszx@ds163510.mlab.com:63510/cine2',
 	SECRET_TOKEN: 'miclavedetokens'
 }
